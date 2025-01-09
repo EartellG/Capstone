d0deb58a9bbc1eb829f12e9935c8c8ff0a7807c2
Sydneii Green <Sydneygreen19@yahoo.com>
2025-01-09 15:31:04 -0600

    Updated

--- components/footer.js
+++ components/footer.js
@@ -1,4 +1,11 @@
 import html from "html-literal";
+() =>
+  html`
+    HTML template literal
+  `;
+
 export default () => html`
-  <footer></footer>
+  <footer>
+    <a href="aboutMe.html">About Navi</a> <a href="contact.html">Contact</a>
+  </footer>
 `;
