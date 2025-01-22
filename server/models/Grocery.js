4e0d705e52852348e7f72e54614273eb721339b1
Sydneii Green <Sydneygreen19@yahoo.com>
2025-01-21 22:25:46 -0600

    Upd2

--- server/models/Grocery.js
+++ server/models/Grocery.js
@@ -3,14 +3,17 @@ import mongoose from "mongoose";
 const grocerySchema = new mongoose.Schema({
   food: {
     type: String,
-    required: true,
-    brand: String,
     validate: /^[A-Za-z0-9 ]*$/
   },
+
   drink: {
     type: String,
-    brand: String,
     validate: /^[A-Za-z0-9 ]*$/
+  },
+
+  brand: {
+    type: String,
+    required: true
   }
 });
 
